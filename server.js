8e94fcf9137bc57727264d7da5ada3f1c9eb55d0
Nick Cooper <nccooper00@gmail.com>
2022-02-01 21:28:28 -0800

    Added server.js and connected main html

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const notes = require('./db/db.json');
+const uuid = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+
+  
+app.listen(PORT, () => 
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
