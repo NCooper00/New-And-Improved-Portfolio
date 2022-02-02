cb0ae07bf0a5ca0558212ac6dc669756d963cebf
Nick Cooper <nccooper00@gmail.com>
2022-02-01 23:07:18 -0800

    server.js resume get

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/script.js', (req, res) => {
 });
 
 app.get('/Extra-Tools/files/Nick+Cooper+Resume+2021-2022.pdf', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Extra-Tools/files/'));
+    res.sendFile(path.join(__dirname, '/Extra-Tools/files/Nick+Cooper+Resume+2021-2022.pdf'));
 });
 
 
