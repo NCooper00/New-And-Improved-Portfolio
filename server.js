b4c508d071258f192b83c71225ea6912a11c10bb
Nick Cooper <nccooper00@gmail.com>
2022-02-02 00:27:00 -0800

    test

--- server.js
+++ server.js
@@ -14,21 +14,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-app.get('/assets/reset.css', (req, res) => {
-    res.sendFile(path.join(__dirname, 'assets/reset.css'));
-});
-
-app.get('/assets/style.css', (req, res) => {
-    res.sendFile(path.join(__dirname, 'assets/style.css'));
-});
-
-// app.get('/script.js', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/script.js'));
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/index.html'));
 // });
+
+// app.get('/assets/reset.css', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'assets/reset.css'));
+// });
+
+// app.get('/assets/style.css', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'assets/style.css'));
+// });
+
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '/script.js'));
+});
 
 // app.get('/Extra-Tools/files/Nick+Cooper+Resume+2021-2022.pdf', (req, res) => {
 //     res.sendFile(path.join(__dirname, '/Extra-Tools/files/Nick+Cooper+Resume+2021-2022.pdf'));
