ab644e1ea56212446b65fea54cddc0a94ff7bf33
Nick Cooper <nccooper00@gmail.com>
2022-02-02 00:04:02 -0800

    added server.js file back and commented out a few things

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-// const express = require('express');
-// const path = require('path');
-// const fs = require('fs');
-// const notes = require('./db/db.json');
-// const uuid = require('uuid');
-// // const resume = require('./Extra-Tools/files/Nick+Cooper+Resume+2021-2022.pdf');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const notes = require('./db/db.json');
+const uuid = require('uuid');
+// const resume = require('./Extra-Tools/files/Nick+Cooper+Resume+2021-2022.pdf');
 
 
 
-// const app = express();
-// const PORT = process.env.PORT || 3001;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/index.html'));
-// });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
 
-// app.get('/assets/reset.css', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/assets/reset.css'));
-// });
+app.get('/assets/reset.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '/assets/reset.css'));
+});
 
-// app.get('/assets/style.css', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/assets/style.css'));
-// });
+app.get('/assets/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '/assets/style.css'));
+});
 
 // app.get('/script.js', (req, res) => {
 //     res.sendFile(path.join(__dirname, '/script.js'));
@@ -36,6 +36,6 @@
 
 
 
-// app.listen(PORT, () => 
-//     console.log(`Example app listening at http://localhost:${PORT}`)
-// );
+app.listen(PORT, () => 
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
